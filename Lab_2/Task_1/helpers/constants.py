82c128d60b14e25129b0c7fb6acb8d40d96c74aa
Vyacheslav <92671408+TestyCore@users.noreply.github.com>
2023-04-05 00:03:29 +0300

    Revert "Lab 2"

--- Lab_2/Task_1/helpers/constants.py
+++
@@ -1,9 +0,0 @@
-ABBREVIATIONS = ["dr.", "jr.", "sr.", "mr.", "Mr.", "ms.", "mrs.", "in.", "ft.",
-                 "etc.", "etc.)",  "e.g.", "i.e.", "a.d.", "b.c.", "c.e.", "b.c.e.",
-                 "inc.", "a.m.", "p.m.", "U.S.A."
-                 ]
-
-PUNCTUATION = [":", ";", "-", ","]
-
-EXTENSIONS = [".zip", ".rar", ".pdf", ".docx", ".txt", ".py", ".dmg", ".pptx",
-              ".cls", ".cpp", ".cs"]
