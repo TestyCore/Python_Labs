3322898a0242f43f6718fa78f6ca4c5fb35263c8
Vyacheslav Tatarinov <tatarinov.slava222@gmail.com>
2023-06-03 14:38:28 +0300

    Added 'home' page

--- Lab_4/edostavka/views.py
+++ Lab_4/edostavka/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Product, ProductCategory, Manufacturer
 
-# Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_products = Product.objects.all().count()
+    num_manufacturers = Manufacturer.objects.all().count()
+
+    # Available books (status = 'a')
+    # num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    # num_authors = Author.objects.count()
+
+    context = {
+        'num_products': num_products,
+        'num_manufacturers': num_manufacturers,
+        # 'num_instances_available': num_instances_available,
+        # 'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
